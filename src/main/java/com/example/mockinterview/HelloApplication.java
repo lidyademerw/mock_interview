e18dc03024f8205543c103ecba13f0cceaf86eaf
lidyademerw <demerwlidya@gmail.com>
2026-02-04 15:55:16 -0800

    "Fix syntax errors and integrate BootstrapFX for styling"

--- src/main/java/com/example/mockinterview/HelloApplication.java
+++ src/main/java/com/example/mockinterview/HelloApplication.java
@@ -1,9 +1,11 @@
 package com.example.mockinterview;
 
+
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import org.kordamp.bootstrapfx.BootstrapFX;
 
 import java.io.IOException;
 
@@ -11,8 +13,9 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        Scene scene = new Scene(fxmlLoader.load(), 400, 600);
+        scene.getStylesheets().add(BootstrapFX.bootstrapFXStylesheet());
+        stage.setTitle("TalkPrep AI");
         stage.setScene(scene);
         stage.show();
     }
--- src/main/java/com/example/mockinterview/HelloController.java
+++ src/main/java/com/example/mockinterview/HelloController.java
@@ -4,11 +4,10 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
 public class HelloController {
-    @FXML
-    private Label welcomeText;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    protected void onStartButtonClick() {
+        System.out.println("Start button clicked! Moving to selection screen...");
+
     }
 }
